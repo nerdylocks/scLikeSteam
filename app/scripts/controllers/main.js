5153830e8f20d5a3092f6a5ba74917c70d843356
aseifu <abiy.seifu@nitropdf.com>
2013-12-19 01:20:15 -0800

    initial commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,10 +5,9 @@ LikeStream.controller('UserMgmt', function($scope, $rootScope, $http, $browser, 
 
     var API_ENDPOINT = 'https://api.soundcloud.com/';
     var me = 'me.json?oauth_token=';
-    //var users = 'ueser?oauth_token=';
-    $cookies.access_token;
+    var users = 'ueser?oauth_token=';
 
-    $scope.LogIn = function () {
+    $rootScope.LogIn = function () {
         OAuth.popup('soundcloud', function (error, result) {
             //use result.access_token in your API request
             if (error) {
@@ -17,10 +16,12 @@ LikeStream.controller('UserMgmt', function($scope, $rootScope, $http, $browser, 
                 // Get access token, store it in cookies
                 $cookies.access_token = result.access_token;
                 $rootScope.GetUserProfile(result.access_token);
+
             }
         });
     }
-    $scope.LogOut = function(){
+
+    $rootScope.LogOut = function(){
         $cookieStore.remove($cookies.access_token);
         $cookieStore.remove("access_token");
         window.location = '/';
@@ -31,6 +32,7 @@ LikeStream.controller('UserMgmt', function($scope, $rootScope, $http, $browser, 
             $cookieStore.put(access_token, data);
             $rootScope.User = $cookieStore.get(access_token);
             console.log($rootScope.User);
+            window.location = '/';
         });
     }
 
@@ -38,11 +40,16 @@ LikeStream.controller('UserMgmt', function($scope, $rootScope, $http, $browser, 
 
     $rootScope.User =  $cookieStore.get($cookies.access_token);
     console.log($rootScope.User);
-
-
+    console.log($cookies.access_token);
 });
-LikeStream.controller('MainCtrl', function ($scope, $http, $rootScope) {
+LikeStream.controller('MainCtrl', function ($scope, $http, $rootScope, $cookies, $cookieStore) {
+    if($rootScope.User){
+        $http.get($rootScope.User.uri + '/followings.json?oauth_token=' + $cookies.access_token).success(function(data){
+            $scope.followers = data;
+        });
+    }
 
+    //console.log($rootScope.User.uri);
 });
 
 LikeStream.controller('LoggedIn', function ($scope) {
